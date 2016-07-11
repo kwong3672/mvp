6e8355b56df49849d7fa50f78f505d81f90331e9
Keith Wong <keith.wong3672@gmail.com>
2016-07-11 12:19:01 -0700

    Added several helper modules created an init file

--- babyMon/server/app.js
+++ babyMon/server/app.js
@@ -1,6 +1,9 @@
 var express = require('express');
-var requestHandler = require('./requestHandler.js');
 var app = express();
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var _ = require('underscore');
+var mongoose = require('mongoose');
 
 var port = 3000;
 app.use(express.static(__dirname + '/public'));
@@ -13,3 +16,4 @@ app.get('/', function(req, res) {
 app.listen(port, function() {
   console.log('Server running on port: ', port);
 });
+
---
+++ babyMon/server/javascripts/init.js
@@ -0,0 +1,7 @@
+var fs = Promise.promisifyAll(require('fs'));
+var _ = require('underscore');
+var mongoose = require('mongoose');
+
+exports.initialize = function(){
+  
+}
