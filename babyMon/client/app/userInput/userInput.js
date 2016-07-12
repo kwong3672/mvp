55f8dec1337f701bb313c60f6ac9c36c99d8125e
Keith Wong <keith.wong3672@gmail.com>
2016-07-11 17:24:25 -0700

    Allow user to select Gender and Age displaying the user's selection on top

--- babyMon/client/app/userInput/userInput.js
+++ babyMon/client/app/userInput/userInput.js
@@ -1,4 +1,18 @@
 angular.module('userInput', [])
   .controller('UserInputController', function($scope) {
+    // create array for possible ages
+    $scope.ageRange = [];
+    for (var i = 1; i < 25; i++) {
+      $scope.ageRange.push(i);
+    }
 
+    $scope.genderSelection = function(gender) {
+      console.log('gender has been selected: ', gender);
+      $scope.displayGender = 'Gender: ' + gender;
+    };
+
+    $scope.ageSelection = function(age) {
+      console.log('age has been selected: ', age);
+      $scope.displayAge = 'Age: ' + age;
+    }
   });
