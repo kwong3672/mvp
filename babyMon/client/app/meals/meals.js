88f63bd50264ab11ce46e31da1168fbb60488347
Keith Wong <keith.wong3672@gmail.com>
2016-07-12 15:01:54 -0700

    Uncommented meals files

--- babyMon/client/app/meals/meals.js
+++ babyMon/client/app/meals/meals.js
@@ -1,29 +1,29 @@
-// var meals = angular.module('meals', [])
-//   .controller('MealsController', function($scope) {
+var meals = angular.module('meals', [])
+  .controller('MealsController', function($scope) {
 
-//   })
-//   .service('meal', function($scope) {
-//     this.submit = function($http) {
-//       return $http({
-//         url: 'http://127.0.0.1:3000/food',
-//         method: 'POST',
-//         contentType: 'application/json',
-//         data: JSON.stringify({
-//           food: $scope.food,
-//           qty: $scope.qty,
-//           month: new Date().getMonth(),
-//           date: new Date().getDAte(),
-//           year: new Date().getFullYear()
-//         }),
-//         success: function(data) {
-//           console.log('success', data);
-//         },
-//         error: function(err) {
-//           console.log('error', error);
-//         }
-//       });
-//     };
-//   });
+  })
+  .service('meal', function($scope) {
+    this.submit = function($http) {
+      return $http({
+        url: 'http://127.0.0.1:3000/food',
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+          food: $scope.food,
+          qty: $scope.qty,
+          month: new Date().getMonth(),
+          date: new Date().getDAte(),
+          year: new Date().getFullYear()
+        }),
+        success: function(data) {
+          console.log('success', data);
+        },
+        error: function(err) {
+          console.log('error', error);
+        }
+      });
+    };
+  });
     
 
 
