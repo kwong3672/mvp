65606ecab7b827712d8dfbffcf0f3cf4e2cb002d
Keith Wong <keith.wong3672@gmail.com>
2016-07-12 15:02:57 -0700

    Uncommented sleep files

--- babyMon/client/app/sleep/sleep.js
+++ babyMon/client/app/sleep/sleep.js
@@ -1,20 +1,20 @@
-// angular.module('sleep', [])
-//   .controller('SleepController', function($scope) {
+angular.module('sleep', [])
+  .controller('SleepController', function($scope) {
 
 
-//     $scope.submit = function() {
-//       $.ajax({
-//         url: 'http://127.0.0.1:3000/sleep/',
-//         method: 'POST',
-//         contentType: 'application/json',
-//         dataType: 'json',
-//         data: 'some stringified object',
-//         success: function(data) {
-//           console.log('success', data);
-//         },
-//         error: function(error) {
-//           console.log('error', error);
-//         }
-//       });
-//     };
-//   });
+    $scope.submit = function() {
+      $.ajax({
+        url: 'http://127.0.0.1:3000/sleep/',
+        method: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: 'some stringified object',
+        success: function(data) {
+          console.log('success', data);
+        },
+        error: function(error) {
+          console.log('error', error);
+        }
+      });
+    };
+  });
